2dd0eddac778330d0fd8ca29ff5cdde4c8aa8759
mwytock0812 <michael.wytock@gmail.com>
2016-04-14 12:22:05 -0400

    Enable CORS and add parsing.

--- backend/app.py
+++ backend/app.py
@@ -16,6 +16,14 @@ define("debug", default=False, help="run in debug mode")
 
 class MainHandler(tornado.web.RequestHandler):
     """Handles post requests by responding with a JSON file."""
+    def set_default_headers(self):
+        self.set_header('Access-Control-Allow-Origin', '*')
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+        self.set_header('Access-Control-Max-Age', 1000)
+        #self.set_header('Access-Control-Allow-Headers', 'origin, x-csrftoken, content-type, accept')
+        self.set_header('Access-Control-Allow-Headers', '*')
+        self.set_header('Content-type', 'application/json')
+
     @tornado.web.asynchronous
     def get(self):
         """Respond to GET requests for debugging purposes."""
@@ -26,7 +34,7 @@ class MainHandler(tornado.web.RequestHandler):
     def post(self):
         """Respond to POST requests with optimal allocations."""
         data = json.loads(self.request.body.decode('utf-8'))
-        stock_params = dict(data)
+        stock_params = dict_from_data(data)
         allocs = optimize_allocations(stock_params)
         self.write(allocs)
         self.finish()
@@ -44,6 +52,21 @@ def optimize_allocations(stock_params):
     prices = get_data(stock_params)
     allocs = optimize_portfolio(prices)
     return allocs
+
+
+def dict_from_data(data):
+    """Maps data from POST request to a Python dictionary"""
+    output = {}
+    data = dict(data)
+    mapping = {"startDate": "start_date",
+               "endDate": "end_date",
+               "initialInvestment": "principle"}
+    for k, v in data.items():
+        if k in mapping:
+            output[mapping[k]] = v
+        else:
+            output[k] = v
+    return output
 
 
 def main():
