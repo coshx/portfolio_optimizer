2edbfe8dbbef1cecd98782bfc9426a549868019e
mwytock0812 <michael.wytock@gmail.com>
2016-05-13 11:41:51 -0400

    More renaming of router component.

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,2 +1,2 @@
 export { environment } from './environment';
-export { PortfolioOptimizerAppComponent } from './portfolio-optimizer.component';
+export { StocksAppComponent } from './stocks-app.component';
