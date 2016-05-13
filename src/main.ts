d778c6f7a0cb0106a5ac7938620c702ccd12b928
mwytock0812 <michael.wytock@gmail.com>
2016-05-13 11:40:50 -0400

    Rename root component selector.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import { enableProdMode } from '@angular/core';
-import { PortfolioOptimizerAppComponent, environment } from './app/';
+import { StocksAppComponent, environment } from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(PortfolioOptimizerAppComponent, [
+bootstrap(StocksAppComponent, [
   ROUTER_PROVIDERS
 ]);
