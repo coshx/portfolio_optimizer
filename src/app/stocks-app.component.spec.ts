0709f6c2bb5c37cca0bf2fe01170b064f091a768
mwytock0812 <michael.wytock@gmail.com>
2016-05-16 15:12:22 -0400

    Fix testing router components.

--- src/app/stocks-app.component.spec.ts
+++ src/app/stocks-app.component.spec.ts
@@ -3,22 +3,35 @@ import {
   describe,
   expect,
   it,
-  inject
+  inject,
 } from '@angular/core/testing';
+import { Location } from '@angular/common';
+import { SpyLocation } from '@angular/common/testing';
+import { provide } from '@angular/core';
+import { Router } from '@angular/router';
+import { ROUTER_PRIMARY_COMPONENT, RouteRegistry } from '@angular/router-deprecated';
+import { RootRouter } from '@angular/router-deprecated/src/router';
 
 import { StocksAppComponent } from './stocks-app.component';
 
-beforeEachProviders(() => [StocksAppComponent]);
+beforeEachProviders(() => [
+  StocksAppComponent,
+  Location,
+  RouteRegistry,
+  provide(Location, {useClass: SpyLocation}),
+  provide(ROUTER_PRIMARY_COMPONENT, {useValue: StocksAppComponent}),
+  provide(Router, {useClass: RootRouter})
+]);
 
 // Test just component methods and attributes
-describe('The StocksAppComponent.', () => {
-  it('should create the stocks app',
+describe('The StocksAppComponent', () => {
+  it('should create the stocks app.',
      inject([StocksAppComponent], (app: StocksAppComponent) => {
        expect(app).toBeTruthy();
      }));
 
-  // it('should have an attribute that contains application routes.',
-  //    inject([StocksAppComponent], (app: StocksAppComponent) => {
-  //      expect(app.appRoutes).toBeTruthy();
-  //    }));
+  it('should contain "Coshx Finance Tools" as the navbar title.',
+     inject([StocksAppComponent], (app: StocksAppComponent) => {
+       expect(app.siteName).toEqual('Coshx Finance Tools');
+     }));
 });
