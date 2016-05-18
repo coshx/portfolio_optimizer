590672d4bdceaed60c6cb7b8084d20e7ef657812
mwytock0812 <michael.wytock@gmail.com>
2016-05-18 11:12:59 -0400

    Add minimal barchart and rename css and html files.

--- src/app/optimizer/input/barchart/barchart.component.ts
+++ src/app/optimizer/input/barchart/barchart.component.ts
@@ -1,15 +1,33 @@
 import {Component, OnInit} from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   moduleId: module.id,
   selector: 'barchart',
-  templateUrl: 'barchart.html',
-  styleUrls: ['barchart.css']
+  templateUrl: 'barchart.component.html',
+  styleUrls: ['barchart.component.css']
 })
 
 export class BarchartComponent implements OnInit {
-  data = [4, 8, 15, 16, 23, 42];
+  constructor() {}
 
   ngOnInit() {
+    this.barchart();
+  }
+
+  barchart() {
+    // TODO: Get data from HTTP response
+    var data = [4, 8, 15, 16, 23, 42];
+
+    var x = d3.scale.linear()
+      .domain([0, d3.max(data)])
+      .range([0, 420]);
+
+    d3.select('.chart')
+      .selectAll('div')
+        .data(data)
+      .enter().append('div')
+        .style('width', function(d) { return x(d) + 'px'; })
+        .text(function(d) { return d; });
   }
 }
