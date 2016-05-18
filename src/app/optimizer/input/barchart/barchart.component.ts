958b02bb6ca9987e266b6f77db3fac2799f69230
mwytock0812 <michael.wytock@gmail.com>
2016-05-18 12:01:11 -0400

    Add Bostock's vertical barchart.

--- src/app/optimizer/input/barchart/barchart.component.ts
+++ src/app/optimizer/input/barchart/barchart.component.ts
@@ -12,22 +12,58 @@ export class BarchartComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    this.barchart();
-  }
+    var margin = {top: 20, right: 30, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-  barchart() {
-    // TODO: Get data from HTTP response
-    var data = [4, 8, 15, 16, 23, 42];
+    var x = d3.scale.ordinal()
+      .rangeRoundBands([0, width], .1);
 
-    var x = d3.scale.linear()
-      .domain([0, d3.max(data)])
-      .range([0, 420]);
+    var y = d3.scale.linear()
+      .range([height, 0]);
 
-    d3.select('.chart')
-      .selectAll('div')
+    var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient('bottom');
+
+    var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient('left')
+      .ticks(10, '%');
+
+    var chart = d3.select('.chart')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    d3.tsv('/app/optimizer/input/barchart/data.tsv', type, function(error, data) {
+      x.domain(data.map(function(d) { return d.letter; }));
+      y.domain([0, d3.max(data, function(d) { return d['frequency']; })]);
+
+      chart.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(xAxis);
+
+      chart.append('g')
+        .attr('class', 'y axis')
+        .call(yAxis);
+
+      chart.selectAll('.bar')
         .data(data)
-      .enter().append('div')
-        .style('width', function(d) { return x(d) + 'px'; })
-        .text(function(d) { return d; });
+        .enter().append('rect')
+        .attr('class', 'bar')
+        .attr('x', function(d) { return x(d['letter']); })
+        .attr('y', function(d) { return y(d['frequency']); })
+        .attr('height', function(d) { return height - y(d['frequency']); })
+        .attr('width', x.rangeBand());
+    });
+
+    function type(d) {
+      d.frequency = +d.frequency; // coerce to number
+      return d;
+    }
   }
+
 }
