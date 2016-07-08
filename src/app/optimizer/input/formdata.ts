de430e004e776c1ace00a0ae5547182caf1376d3
curtisxk38 <ck2ha@virginia.edu>
2016-07-08 12:04:04 -0400

    Use FormGroup instead of ngModel
    This lets us use our validators

--- src/app/optimizer/input/formdata.ts
+++
@@ -1,9 +0,0 @@
-export class FormData {
-	constructor(
-	  public symbols: string,
-    public startDate: string,
-    public endDate: string,
-    public initialInvestment: string
-	) { }
-
-}
--- src/app/optimizer/input/input.component.ts
+++ src/app/optimizer/input/input.component.ts
@@ -1,76 +1,56 @@
 import {Injectable, Component, OnInit, Output, EventEmitter} from '@angular/core';
-/*import {
-  FormBuilder,
-  Validators,
-  Control,
-  ControlGroup,
-  FORM_DIRECTIVES,
-   } from '@angular/common';*/
-import {NgForm} from '@angular/forms';
-import {FormData} from './formdata';
+import {FormGroup, FormControl, Validators, REACTIVE_FORM_DIRECTIVES} from '@angular/forms';
 import {SymbolsValidator} from './symbols.validator';
-import {OptimizerDataService} from '../optimizer-data.service';
 
 @Component({
   moduleId: module.id,
   selector: 'user-input',
   templateUrl: 'input.component.html',
-  styleUrls: ['input.component.css']
+  styleUrls: ['input.component.css'],
+  directives: [REACTIVE_FORM_DIRECTIVES]
 })
 
 @Injectable()
 export class InputComponent implements OnInit {
   
   @Output() onSubmit = new EventEmitter();
-  x: number = 0;
 
-  model = new FormData('AAPL, GOOG, FB', '01/01/2012', '03/20/2016', '1000');
+  optimizeForm: FormGroup;
 
-  constructor() {//public http: Http, private builder: FormBuilder, private optimizerDataService: OptimizerDataService) {
-    /*this.symbols = new Control(
-      'AAPL, GOOG, FB',
-      Validators.compose([Validators.required,
-                          SymbolsValidator.tooFewSymbols])
-    );
-
-    this.startDate = new Control(
-      '01/01/2012',
-      Validators.compose([Validators.required,
-                          Validators.pattern('[0-9]{2}\/[0-9]{2}\/[0-9]{4}')])
-    );
-
-    this.endDate = new Control(
-      '03/20/2016',
-      Validators.compose([Validators.required,
-                          Validators.pattern('[0-9]{2}\/[0-9]{2}\/[0-9]{4}')])
-    );
-
-    this.initialInvestment = new Control(
-      '1000',
-      Validators.compose([Validators.required])
-    );
-
-    this.form = builder.group({
-      symbols: this.symbols,
-      startDate: this.startDate,
-      endDate: this.endDate,
-      initialInvestment: this.initialInvestment
-    });*/
-    //this.model = new FormData('AAPL, GOOG, FB', '01/01/2012', '03/20/2016', '1000')
+  constructor() {
+    this.optimizeForm = new FormGroup({
+      symbols: new FormControl(
+        'AAPL, GOOG, FB', 
+        Validators.compose([Validators.required, SymbolsValidator.tooFewSymbols])
+        ),
+      startDate: new FormControl(
+        '01/01/2012',
+        Validators.compose([Validators.required, Validators.pattern('[0-9]{2}\/[0-9]{2}\/[0-9]{4}')])
+        ),
+      endDate: new FormControl(
+        '03/20/2016',
+        Validators.compose([Validators.required, Validators.pattern('[0-9]{2}\/[0-9]{2}\/[0-9]{4}')])
+        ),
+      initialInvestment: new FormControl(
+        '1000',
+        Validators.compose([Validators.required])
+        )
+    });
   }
-  submitData() {
+  submitData(form) {
     let query = {symbols: [], startDate: '', endDate: '', initialInvestment: ''};
     // Turn string of stocks, into array of strings
     // ex: 'AAPL, GOOG, FB' --> ['AAPL', 'GOOG', 'FB']
-    query.symbols = this.model.symbols.replace(/ /g, '').split(',');
-    query.startDate = this.model.startDate;
-    query.endDate = this.model.endDate;
-    query.initialInvestment = this.model.initialInvestment;
+    // This is the format that the backend requires
+    query.symbols = form.symbols.replace(/ /g, '').split(',');
+    query.startDate = form.startDate;
+    query.endDate = form.endDate;
+    query.initialInvestment = form.initialInvestment;
 
     this.onSubmit.emit(query);
   }
 
-  get diagnostic() { return JSON.stringify(this.model); }
+  get diagnostic() { return JSON.stringify(this.optimizeForm); }
 
   ngOnInit() {
   }
