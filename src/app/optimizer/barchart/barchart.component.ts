4f92127fff0870e4215e4731af7983261268514b
curtisxk38 <ck2ha@virginia.edu>
2016-07-13 11:20:47 -0400

    Change chart creation to set the width equal to the container width

--- src/app/optimizer/barchart/barchart.component.ts
+++ src/app/optimizer/barchart/barchart.component.ts
@@ -16,33 +16,46 @@ export class BarchartComponent{
   constructor(private optimizerDataService: OptimizerDataService) {}
 
   @Input() optimalAllocs: Array<any>;
-  @Input() title;
-  viewInitialized = false;
+  @Input() title: string;
+  viewInitialized: boolean = false;
 
   create() {
-    var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 500 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    let w = document.getElementsByClassName("chart")[0].clientWidth;
+    let margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = w - margin.left - margin.right,
+    height = 384 - margin.top - margin.bottom;
 
-    var x = d3.scale.ordinal()
+    let x = d3.scale.ordinal()
         .rangeRoundBands([0, width], .1);
 
-    var y = d3.scale.linear()
+    let y = d3.scale.linear()
         .range([height, 0]);
 
-    var xAxis = d3.svg.axis()
+    let xAxis = d3.svg.axis()
         .scale(x)
         .orient("bottom");
 
-    var yAxis = d3.svg.axis()
+    let yAxis = d3.svg.axis()
         .scale(y)
         .orient("left");
 
-    var svg = d3.select("div.chart").append("svg")
+    let svg = d3.select("div.chart").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
       .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    /*
+    d3.select("div.chart")
+      .append("div")
+      .classed("svg-container", true);
+
+    let svg = d3.select("div.svg-container").append("svg")
+      //responsive SVG needs these 2 attributes and no width and height attr
+      .attr("preserveAspectRatio", "xMinYMin meet")
+      .attr("viewBox", "0 0 " + width + " " + height)
+      //class to make it responsive
+      .classed("svg-content-responsive", true);*/
 
     x.domain(this.optimalAllocs.map(function(d) { return d.name; }));
     y.domain([0, d3.max(this.optimalAllocs, function(d) { return d.value; })]);
@@ -66,11 +79,11 @@ export class BarchartComponent{
         .attr("height", function(d) { return height - y(d.value); });
   }
   removeOldChart() {
-    let chartDiv = document.getElementsByClassName("chart")[0];
-    chartDiv.removeChild(chartDiv.childNodes[0]);
+    d3.select('svg').remove();
   }
 
   onResize(){
+
   }
 
   ngAfterViewInit(){
@@ -82,7 +95,6 @@ export class BarchartComponent{
     if (this.viewInitialized) {
       this.removeOldChart();
       this.create();
-      //this.updateChart();
     }
     
   }
