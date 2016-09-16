92f9335ab08751c42cf7854d9d7faeca427dfc09
mwytock0812 <michael.wytock@gmail.com>
2016-09-16 11:15:35 -0400

    Move charts component out of charts module.

--- src/app/optimizer/charts/charts.routing.ts
+++ src/app/optimizer/charts/charts.routing.ts
@@ -1,29 +1,22 @@
 import { ModuleWithProviders }   from '@angular/core';
 import { Routes, RouterModule }  from '@angular/router';
 
-import { ChartsComponent } from './charts.component';
 import { BarchartComponent } from './barchart/barchart.component';
 import { LineGraphComponent } from './line-graph/line-graph.component';
 
 const crisisCenterRoutes: Routes = [
   {
-    path: '',
-    redirectTo: '/charts',
+    path: 'optimizer',
+    redirectTo: '/optimizer/line-graph',
     pathMatch: 'full'
   },
   {
-    path: 'charts',
-    component: ChartsComponent,
-    children: [
-      {
-        path: 'line-graph',
-        component: LineGraphComponent,
-      },
-      {
-        path: 'barchart',
-        component: BarchartComponent
-      }
-    ]
+    path: 'line-graph',
+    component: LineGraphComponent,
+  },
+  {
+    path: 'barchart',
+    component: BarchartComponent
   }
 ];
 
