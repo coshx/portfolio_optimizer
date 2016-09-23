3b3da0a3abcf007b1da47cc2cd4c0b749ff88224
mwytock0812 <michael.wytock@gmail.com>
2016-09-23 15:50:44 -0400

    Send JSON formatted as an array of objects from backend.

--- backend/optimizer/optimize.py
+++ backend/optimizer/optimize.py
@@ -128,8 +128,23 @@ def optimize_portfolio(prices, prices_SPY):
     return {'optimal_allocations': {k: v for (k, v) in zip(symbols, allocs)},
             'sharpe_ratio': get_sharpe_ratio(port_val, SPY_val),
             'cumulative_returns': get_cumulative_returns(port_val),
-            'performance': compare_SPY.to_json()}
+            'performance': df_to_d3(compare_SPY)}
 
+def df_to_d3(df):
+    """Turn each column of a dataframe into a d3-ready JSON object.
+
+    Args:
+        df (dataframe): the dataframe whose columns to transform
+
+    Returns:
+        data (dict): a dict with df's column names as keys and
+            df's JSONified rows as values
+    """
+    data = {}
+    for col in df:
+        data[col] = df[col].reset_index().to_json(date_format='iso',
+                                                  orient='records')
+    return data
 
 def main():
     """Driver function."""
